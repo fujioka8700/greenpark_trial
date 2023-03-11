f8b515a5b41b2fab327b4f05ece94ca01f2584bc
fujioka <fujioka8700@gmail.com>
2023-03-11 16:05:40 +0900

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  @includeWhen(config('google.ga_enable'), 'ga')
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>{{ config('app.name') }}</title>
+  @vite(['resources/css/app.css', 'resources/scss/styles.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+  <div id="app"></div>
+</body>
+
+</html>
