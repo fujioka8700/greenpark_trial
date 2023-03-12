699abb625cf9c7a7ad87a79e2637bd8e12b9c113
fujioka <fujioka8700@gmail.com>
2023-03-12 15:08:57 +0900

    [add] auth.js 追加

--- resources/js/components/pages/About.vue
+++ resources/js/components/pages/About.vue
@@ -1,9 +1,38 @@
 <template>
   <div>
-    <h2>About</h2>
+    <h2>AboutView</h2>
+    <p>名前: {{ user.name }}</p>
+    <p>メールアドレス: {{ user.email }}</p>
+    <button type="button" @click="logout">ログアウト</button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const user = ref({ name: "", email: "" });
+
+onMounted(() => {
+  axios
+    .get("/api/user")
+    .then((res) => {
+      user.value = res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+const logout = () => {
+  axios
+    .post("/api/logout")
+    .then((res) => {
+      router.push("/login");
+    })
+    .catch((err) => {});
+};
+</script>
 
 <style lang="scss" scoped></style>
