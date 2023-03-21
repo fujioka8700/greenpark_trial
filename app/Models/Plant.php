1a59135bbeb91f4d91715b3da1a9021a14a69235
fujioka <fujioka8700@gmail.com>
2023-03-21 10:29:39 +0900

    [add] Route::apiResource 追加

--- app/Models/Plant.php
+++ app/Models/Plant.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plant extends Model
 {
-    use HasFactory;
+  use HasFactory;
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\PlantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::post('/register', [RegisterController::class, 'register']);
+
+Route::apiResource('plant', PlantController::class);
