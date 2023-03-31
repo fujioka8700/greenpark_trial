babc5ac6a94448a19f693f253808c5dee22fb871
fujioka <fujioka8700@gmail.com>
2023-04-01 07:29:04 +0900

    [update] Plant Factory 定数を設定。

--- database/factories/PlantFactory.php
+++ database/factories/PlantFactory.php
@@ -10,6 +10,10 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class PlantFactory extends Factory
 {
+  const IMAGE_WIDTH = 640;  // テスト画像の幅
+  const IMAGE_HEIGHT = 640; // テスト画像の高さ
+  const WORD_COUNT = 10;    // 植物名の文字上限
+
   /**
    * Define the model's default state.
    *
@@ -17,11 +21,15 @@ class PlantFactory extends Factory
    */
   public function definition(): array
   {
-    $path = fake()->image(storage_path('app/public/images'), 640, 480);
+    $path = fake()->image(
+      storage_path('app/public/images'),
+      self::IMAGE_WIDTH,
+      self::IMAGE_HEIGHT
+    );
     $path = preg_replace('/(.*)public/', '/storage', $path);
 
     return [
-      'name' => fake()->realText(10),
+      'name' => fake()->realText(self::WORD_COUNT),
       'file_path' => $path,
       'user_id' => User::factory(),
     ];
