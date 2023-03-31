c34941546547e17874015269634f7fae43515b9c
fujioka <fujioka8700@gmail.com>
2023-04-01 07:04:16 +0900

    [update] User, Plant Model 内のメソッド、返り値の型を指定。

--- app/Models/Plant.php
+++ app/Models/Plant.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,7 +24,7 @@ class Plant extends Model
    * 多対1のplantsテーブル(子テーブル)でusersテーブル(親テーブル)のリレーションを定義
    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
    */
-  public function user()
+  public function user(): BelongsTo
   {
     return $this->belongsTo(User::class);
   }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Plant;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -47,7 +48,7 @@ class User extends Authenticatable
    * 1対多のusersテーブル(親テーブル)でplantsテーブル(子テーブル)のリレーションを定義
    * @return \Illuminate\Database\Eloquent\Relations\HasMany
    */
-  public function plants()
+  public function plants(): HasMany
   {
     return $this->hasMany(Plant::class);
   }
