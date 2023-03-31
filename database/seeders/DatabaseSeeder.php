6ddc10549ba424f4284ed6a58d1cf4e24638ecff
fujioka <fujioka8700@gmail.com>
2023-04-01 07:58:44 +0900

    [update] DatabaseSeeder 定数とコメントの追加。

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+  const USERS_COUNT = 5;   // 作成するユーザー数
+  const PLANTS_COUNT = 10; // 作成する植物数
+
   /**
    * Seed the application's database.
    */
@@ -21,16 +24,20 @@ class DatabaseSeeder extends Seeder
     //     'email' => 'test@example.com',
     // ]);
 
+    /** ログインしやすいユーザーの作成 */
     $user = User::factory()->create([
       'name' => 'Test User',
       'email' => 'test@example.com',
       'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
     ]);
 
-    $users = User::factory(5)->create();
+    /** 5ユーザー作成 */
+    $users = User::factory(self::USERS_COUNT)->create();
 
+    /** ログインしやすいユーザーを1番目に追加 */
     $users->prepend($user);
 
-    Plant::factory(10)->recycle($users)->create();
+    /** ユーザーとリレーションした、植物を作成 */
+    Plant::factory(self::PLANTS_COUNT)->recycle($users)->create();
   }
 }
