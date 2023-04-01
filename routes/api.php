55d74f7b4d17a48568df9cd0df86ddf69e910bf8
fujioka <fujioka8700@gmail.com>
2023-04-01 10:48:59 +0900

    [refactor] api.php Docを追記した。

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\PlantController;
 |
 */
 
+/** ログイン中ユーザーの情報を取得する */
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
 });
@@ -25,6 +26,7 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::post('/register', [RegisterController::class, 'register']);
 
+/** キーワードを入力して植物を検索する */
 Route::get('/plants/search', [PlantController::class, 'search']);
 
 Route::apiResource('plants', PlantController::class);
