b70c7b102cacf0943385e54d2ec57749952d1372
fujioka <fujioka8700@gmail.com>
2023-04-15 19:17:23 +0900

    [add] ホームをクリックした時、パンくずリストをリセットする

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -6,7 +6,19 @@
 </template>
 
 <script setup>
+import { useStoreBreadCrumbs } from "../../store/breadCrumbs";
+import { onMounted } from "vue";
 import SearchPlants from "../modules/SearchPlants.vue";
+
+// パンくずリストはStoreに保存している
+const breadCrumbs = useStoreBreadCrumbs();
+
+onMounted(() => {
+  // パンくずリストをリセットする
+  if (breadCrumbs.items) {
+    breadCrumbs.$reset();
+  }
+});
 </script>
 
 <style lang="scss" scoped></style>
