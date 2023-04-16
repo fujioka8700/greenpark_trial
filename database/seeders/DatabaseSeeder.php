7d7abb779531668365621849684c854844fc196a
fujioka <fujioka8700@gmail.com>
2023-04-16 20:46:19 +0900

    [refactor] DatabaseSeeder.php の整頓をした。

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,25 +13,16 @@ class DatabaseSeeder extends Seeder
 {
   const USERS_COUNT = 5; // 作成するユーザー数
   const PLANTS_COUNT = 3; // 作成する植物数
-  const COLORS_COUNT = 9; // 色の数
 
   /**
    * Seed the application's database.
    */
   public function run(): void
   {
-    // \App\Models\User::factory(10)->create();
-
-    // \App\Models\User::factory()->create([
-    //     'name' => 'Test User',
-    //     'email' => 'test@example.com',
-    // ]);
-
     /** ログインしやすいユーザーの作成 */
     $user = User::factory()->create([
       'name' => 'Test User',
       'email' => 'test@example.com',
-      'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
     ]);
 
     /** ユーザー作成 */
@@ -41,7 +32,6 @@ class DatabaseSeeder extends Seeder
     $users->prepend($user);
 
     /** 植物の色 */
-    // $colors = Color::factory(self::COLORS_COUNT)->create();
     $colors = Color::all();
 
     /** 植物の生育場所 */
@@ -51,7 +41,6 @@ class DatabaseSeeder extends Seeder
      * 1対多 ユーザー 対 植物
      * 植物を作成するユーザーはランダム
      */
-    // Plant::factory(self::PLANTS_COUNT)->hasAttached($colors)->recycle($users)->create();
     Plant::factory(self::PLANTS_COUNT)
       ->recycle($users)
       ->create()
@@ -64,6 +53,7 @@ class DatabaseSeeder extends Seeder
           $plant
             ->colors()
             ->attach($colors->random(random_int(1, $colors->count())));
+
           /**
            * 多対多 植物 対 生育場所
            * 植物の生育場所はランダム(街路～社寺)
