9d222771e4b66a65218268ebaa9f0e1f7be07add
fujioka <fujioka8700@gmail.com>
2023-04-22 15:22:55 +0900

    [update] PlantPlaces.vue から生育場所で、検索するメソッドを追加した。

--- resources/js/components/modules/PlantPlaces.vue
+++ resources/js/components/modules/PlantPlaces.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <ul>
-      <li>街路・生け垣</li>
-      <li>市街地・公園</li>
-      <li>神社・寺院</li>
+      <li>
+        <a href="" @click.stop.prevent="createSearchPlaces">街路・生け垣</a>
+      </li>
+      <li>
+        <a href="" @click.stop.prevent="createSearchPlaces">市街地・公園</a>
+      </li>
+      <li>
+        <a href="" @click.stop.prevent="createSearchPlaces">神社・寺院</a>
+      </li>
       <li>道端・草地</li>
       <li>空き地・土手</li>
       <li>田畑・あぜ</li>
@@ -13,6 +19,40 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+/** @type {Array} 検索する生育場所 */
+const places = ref([]);
+
+/**
+ * 生育場所で植物を検索する
+ * @param {Array} places 検索する生育場所
+ */
+const searchPlaces = (places) => {
+  axios
+    .get("/api/plants/search-places", {
+      params: {
+        places,
+      },
+    })
+    .then((result) => {
+      console.log(result);
+    });
+};
+
+/**
+ * 生育場所を作成後、検索する
+ * @param {Object} event クリックした li 要素
+ */
+const createSearchPlaces = (event) => {
+  const string = event.target.innerText;
+
+  /** @type {Array} 生育場所 */
+  const places = string.split("・");
+
+  searchPlaces(places);
+};
+</script>
 
 <style lang="scss" scoped></style>
