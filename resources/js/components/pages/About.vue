ae3ab276775bb0e0fc967f1cf5d68bfdd419cb15
fujioka <fujioka8700@gmail.com>
2023-05-06 18:10:07 +0900

    [update] About.vue ユーザー情報ページをデザインした。

--- resources/js/components/pages/About.vue
+++ resources/js/components/pages/About.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>
-    <h2>AboutView</h2>
-    <p>名前: {{ user.name }}</p>
-    <p>メールアドレス: {{ user.email }}</p>
-    <button type="button" @click="auth.logout">ログアウト</button>
+  <div class="mt-10">
+    <v-card class="mx-auto" max-width="344">
+      <v-img height="200px" cover class="text-center p-about-avatar-bg">
+        <v-avatar class="p-about-avatar" color="surface-variant" size="100">
+          <v-icon icon="mdi-account" size="90"></v-icon>
+        </v-avatar>
+      </v-img>
+
+      <v-card-title class="text-center">{{ user.name }}&nbsp;様</v-card-title>
+
+      <v-card-text class="text-center">
+        <v-icon icon="mdi-email"></v-icon>&nbsp;{{ user.email }}
+      </v-card-text>
+
+      <v-card-actions>
+        <div class="mx-auto mb-6">
+          <v-btn
+            color="green-lighten-2"
+            variant="outlined"
+            @click="auth.logout"
+          >
+            ログアウト
+          </v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -31,4 +52,16 @@ onMounted(async () => {
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.p-about-avatar {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  &-bg {
+    background-image: linear-gradient(#89f7fe 0%, #66a6ff 100%);
+    position: relative;
+  }
+}
+</style>
