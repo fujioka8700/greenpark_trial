d9d8cb098629470fd205db427270ac514f513539
fujioka <fujioka8700@gmail.com>
2023-05-15 23:26:18 +0900

    [update] 植物登録画面の、デザインを変更した。

--- resources/js/components/pages/StorePlant.vue
+++ resources/js/components/pages/StorePlant.vue
@@ -1,128 +1,122 @@
 <template>
   <div>
-    <h2>植物登録</h2>
-    <ul>
-      <li v-for="message in errorMessages">{{ message }}</li>
-    </ul>
-    <form @submit.prevent="sendButton">
-      <label>
-        <input type="text" v-model="name" placeholder="name" />
-      </label>
-      <br />
-      <label>
-        <input type="file" @change="fileSelected" />
-      </label>
-      <br />
-      <p>よく生えている場所</p>
-      <ul>
-        <li v-for="place in places" :key="place.id">
-          <input
-            type="checkbox"
-            :id="place.id"
-            :value="place.id"
-            v-model="placesCheckedValues"
-          />
-          <label :for="place.id">{{ place.name }}</label>
-        </li>
-      </ul>
-      <p>花の色</p>
-      <ul>
-        <li v-for="color in colors" :key="color.id">
-          <input
-            type="checkbox"
-            :id="color.id"
-            :value="color.id"
-            v-model="colorsCheckedValues"
-          />
-          <label :for="color.id">{{ color.name }}</label>
-        </li>
-      </ul>
-      <label>
-        <textarea
-          v-model="description"
-          cols="30"
-          rows="10"
-          placeholder="説明"
-        ></textarea>
-      </label>
-      <br />
-      <button type="submit">登録する</button>
-    </form>
-  </div>
-  <div>
-    <v-form @submit.prevent="sendButton">
-      <v-text-field
-        v-model="name"
-        type="text"
-        label="名前"
-        :rules="[required]"
-        :error="errors.name"
-        :error-messages="errorMessages.name"
-        clearable
-        @keydown="clearError('name')"
-      ></v-text-field>
-      <v-file-input
-        v-model="image"
-        clearable
-        accept="image/*"
-        label="ファイルを選んでください"
-        :error="errors.file"
-        :error-messages="errorMessages.file"
-        @change="changeFile"
-      ></v-file-input>
-      <v-img :src="url" width="100"></v-img>
-      <div>
-        <p>よく生えている場所</p>
-        <div v-if="errorMessages.places">
-          <p class="text-error text-caption">
-            {{ errorMessages.places[0] }}
-          </p>
+    <v-sheet
+      elevation="3"
+      max-width="800"
+      rounded
+      width="100%"
+      class="pa-4 px-6 ma-4 mx-auto"
+    >
+      <h2 class="text-h5 pt-2 text-center">植物を登録する</h2>
+      <v-divider class="my-5 mx-7"></v-divider>
+      <v-form v-model="form" @submit.prevent="sendButton">
+        <p>名前</p>
+        <v-text-field
+          v-model="name"
+          type="text"
+          label="名前を入力してください"
+          :rules="[required]"
+          :error="errors.name"
+          :error-messages="errorMessages.name"
+          clearable
+          @keydown="clearError('name')"
+        ></v-text-field>
+        <p>植物の写真</p>
+        <v-file-input
+          v-model="image"
+          clearable
+          accept="image/*"
+          label="ファイルを選んでください"
+          :error="errors.file"
+          :error-messages="errorMessages.file"
+          @change="changeFile"
+        ></v-file-input>
+        <v-sheet
+          color="grey-lighten-4"
+          rounded
+          class="mx-auto mb-7 d-flex align-center justify-center pa-1"
+          height="150"
+          width="250"
+        >
+          <template v-if="url">
+            <v-img :src="url" max-width="250"></v-img>
+          </template>
+          <template v-else>
+            <p class="text-body-2">ここに植物の写真が表示されます</p>
+          </template>
+        </v-sheet>
+        <div>
+          <p>よく生えている場所</p>
+          <div v-if="errorMessages.places">
+            <p class="text-error text-caption">
+              {{ errorMessages.places[0] }}
+            </p>
+          </div>
+          <v-container fluid>
+            <v-row>
+              <v-col
+                v-for="place in places"
+                :key="place.id"
+                cols="4"
+                sm="3"
+                md="3"
+              >
+                <v-checkbox
+                  v-model="placesCheckedValues"
+                  :label="place.name"
+                  :value="place.id"
+                  :error="errors.places"
+                  @change="clearError('places')"
+                ></v-checkbox>
+              </v-col>
+            </v-row>
+          </v-container>
         </div>
-        <v-container fluid>
-          <v-row>
-            <v-col
-              v-for="place in places"
-              :key="place.id"
-              cols="4"
-              sm="3"
-              md="3"
-            >
-              <v-checkbox
-                v-model="placesCheckedValues"
-                :label="place.name"
-                :value="place.id"
-                :error="errors.places"
-                @change="clearError('places')"
-              ></v-checkbox>
-            </v-col>
-          </v-row>
-        </v-container>
-      </div>
-      <div>
-        <p>花の色</p>
-        <v-container fluid>
-          <v-row>
-            <v-col
-              v-for="color in colors"
-              :key="color.id"
-              cols="4"
-              sm="3"
-              md="3"
-            >
-              <v-checkbox
-                v-model="colorsCheckedValues"
-                :label="color.name"
-                :value="color.id"
-              ></v-checkbox>
-            </v-col>
-          </v-row>
-        </v-container>
-      </div>
+        <div>
+          <p>花の色</p>
+          <v-container fluid>
+            <v-row>
+              <v-col
+                v-for="color in colors"
+                :key="color.id"
+                cols="4"
+                sm="3"
+                md="3"
+              >
+                <v-checkbox
+                  v-model="colorsCheckedValues"
+                  :label="color.name"
+                  :value="color.id"
+                ></v-checkbox>
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
+        <div>
+          <p>説明</p>
+          <v-textarea
+            v-model="description"
+            clearable
+            clear-icon="mdi-close-circle"
+            label="説明を入力してください"
+            :rules="[required]"
+            :error="errors.description"
+            :error-messages="errorMessages.description"
+            @keydown="clearError('description')"
+          ></v-textarea>
+        </div>
 
-      <v-btn type="submit" block color="light-blue-darken-1" class="mt-2">
-        植物を登録する
-      </v-btn>
-    </v-form>
+        <v-btn
+          type="submit"
+          block
+          :disabled="!form"
+          color="light-blue-darken-1"
+          class="mt-2"
+        >
+          植物を登録する
+        </v-btn>
+      </v-form>
+    </v-sheet>
   </div>
 </template>
 
@@ -167,6 +161,9 @@ const placesCheckedValues = ref([]);
 
 /** @type {Object} 植物の写真 */
 const image = ref(null);
+
+/** @type {boolean} フォームの入力確認 */
+const form = ref(false);
 
 /**
  * バリデーション、必須入力
