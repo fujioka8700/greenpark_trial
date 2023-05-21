0bae66046aef9fd28a0056095deffc18e8a90f79
fujioka <fujioka8700@gmail.com>
2023-05-21 18:41:48 +0900

    [update] 注目の植物たちに、本物の植物を表示した。

--- resources/js/components/modules/Recommend.vue
+++ resources/js/components/modules/Recommend.vue
@@ -1,38 +1,37 @@
 <template>
-  <div v-for="(img, index) in ary" key="index">
+  <div v-for="(plant, index) in recommendPlants" :key="index">
     <RouterLink
       class="text-decoration-none"
-      :to="{ name: 'PlantItem', params: { plantId: index } }"
+      :to="{ name: 'PlantItem', params: { plantId: plant.id } }"
     >
       <v-card link flat rounded="0" class="d-flex py-4">
         <v-img
-          src="https://picsum.photos/300/200"
-          min-width="100"
+          :src="`../${plant.file_path}`"
+          min-width="120"
           max-width="120"
+          height="80"
           class="ml-2"
           cover
         ></v-img>
         <div class="pl-1">
-          <p>植物タイトル</p>
+          <p>{{ plant.name }}</p>
           <p class="pl-1">
-            {{
-              omittedText20(
-                "解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説解説"
-              )
-            }}
+            {{ omittedText20(plant.description) }}
           </p>
         </div>
       </v-card>
     </RouterLink>
-    <template v-if="index !== ary.length - 1">
+    <template v-if="index !== recommendPlants.length - 1">
       <v-divider />
     </template>
   </div>
 </template>
 
 <script setup>
-/** @type {Array} （仮）注目の植物たち */
-const ary = Array(5);
+import { onMounted, ref } from "vue";
+
+/** @type {Object} 注目の植物たち */
+const recommendPlants = ref({});
 
 /**
  * 20字以上の説明は、3点リーダーにする
@@ -42,6 +41,19 @@ const ary = Array(5);
 const omittedText20 = (text) => {
   return text.length > 20 ? text.slice(0, 20) + "..." : text;
 };
+
+/**
+ * 「注目の植物たち」を取得する
+ */
+const fetchRecommendedPlants = () => {
+  axios.get(`/api/plants/recommend`).then((result) => {
+    recommendPlants.value = result.data;
+  });
+};
+
+onMounted(() => {
+  fetchRecommendedPlants();
+});
 </script>
 
 <style lang="scss" scoped></style>
