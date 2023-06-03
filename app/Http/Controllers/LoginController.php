57a8f90978dd0f5588253c0d8310be37f6890fed
fujioka <fujioka8700@gmail.com>
2023-06-04 08:03:17 +0900

    [refactor] LoginController, LoginTest, UserRequest リファクタリングを行った。

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,15 +12,12 @@ class LoginController extends Controller
   /**
    * ユーザーをログインする。
    * ログインできない場合は、ステータスコード500を返す。
-   * @param \Illuminate\Http\Request $request
+   * @param \Illuminate\Http\UserRequest $request
    * @return \Illuminate\Http\JsonResponse
    */
-  public function login(Request $request): JsonResponse
+  public function login(UserRequest $request): JsonResponse
   {
-    $credentials = $request->validate([
-      'email' => 'required|email',
-      'password' => 'required',
-    ]);
+    $credentials = $request->input();
 
     if (Auth::attempt($credentials)) {
       return response()->json([
