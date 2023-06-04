633e0a79e5491aa6499e5af25ee0285a6aff01c6
fujioka <fujioka8700@gmail.com>
2023-06-04 19:04:13 +0900

    [refactor] Place, PlaceController リファクタリングを行った。

--- tests/Feature/PlaceControllerTest.php
+++ tests/Feature/PlaceControllerTest.php
@@ -2,9 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
-use App\Models\Plant;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,24 +9,10 @@ use Tests\TestCase;
 class PlaceControllerTest extends TestCase
 {
   use RefreshDatabase;
-  use WithFaker;
 
   protected function setUp(): void
   {
     parent::setUp();
-
-    Storage::fake('local');
-
-    // ユーザー1人、作成
-    $user = User::factory()->create();
-
-    // 良く生えている場所1つとリレーションした、植物を作成
-    Plant::factory(1)
-      ->recycle($user)
-      ->create()
-      ->each(function ($plant) {
-        $plant->places()->attach(1);
-      });
   }
 
   public function test_良く生えている場所、一覧を取得する(): void
