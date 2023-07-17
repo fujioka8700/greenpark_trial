aff8b68ea21427df26669cbc70299f395ab4980c
fujioka <fujioka8700@gmail.com>
2023-07-18 07:14:21 +0900

    [fix] 未ログインなら、コメント入力を非表示にした。

--- resources/js/components/modules/CommentField.vue
+++ resources/js/components/modules/CommentField.vue
@@ -1,25 +1,35 @@
 <template>
-  <v-form @submit.prevent="sendButton">
-    <v-text-field
-      v-model="comment"
-      prepend-inner-icon="mdi-pencil"
-      variant="underlined"
-      clearable
-    ></v-text-field>
-    <div class="text-right mb-5">
-      <v-btn
-        type="submit"
-        :disabled="!comment"
-        color="blue-lighten-1"
-        rounded="xl"
-        >コメント</v-btn
-      >
-    </div>
-  </v-form>
+  <div v-if="!!user">
+    <v-form @submit.prevent="sendButton">
+      <v-text-field
+        v-model="comment"
+        prepend-inner-icon="mdi-pencil"
+        variant="underlined"
+        clearable
+      ></v-text-field>
+      <div class="text-right mb-5">
+        <v-btn
+          type="submit"
+          :disabled="!comment"
+          color="blue-lighten-1"
+          rounded="xl"
+          >コメント</v-btn
+        >
+      </div>
+    </v-form>
+  </div>
+  <div v-else class="ma-8">
+    <!-- コメント入力、非表示なら、余白を挿入する -->
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { useStoreAuth } from "../../store/auth";
+import { storeToRefs } from "pinia";
+
+const auth = useStoreAuth();
+const { user } = storeToRefs(auth);
 
 /** @type {string} 送信する前のコメント */
 const comment = ref("");
