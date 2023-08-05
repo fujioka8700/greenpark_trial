18980d7df665717e57520b24521655dbb8de27b7
fujioka <fujioka8700@gmail.com>
2023-08-05 17:27:49 +0900

    [add] TOPページから「花の色」で検索できるようにした。

--- resources/js/components/modules/PlantColors.vue
+++ resources/js/components/modules/PlantColors.vue
@@ -1,7 +1,12 @@
 <template>
   <v-row>
     <v-col v-for="color in plantColors" :key="color" cols="12" sm="4">
-      <v-card class="mx-auto" max-width="344" link>
+      <v-card
+        class="mx-auto"
+        max-width="344"
+        link
+        @click="chooseFlowerColor(color)"
+      >
         <v-img
           src="https://lovegreen.net/wp-content/uploads/2020/12/c8c593c9d3528f2d0e0803a375d0bfc7.jpg"
           max-height="130"
@@ -16,26 +21,39 @@
 
 <script setup>
 import { plantColors } from "../../util";
+import { inject } from "vue";
+
+const changeColorResults = inject("changeColorResults");
 
 /**
- * 花の色で検索
+ * 選択した色から、植物一覧のデータを受け取る
+ * @param {Array} colors
  */
-const searchFlowerColor = () => {
+const getFlowerColorList = (colors) => {
   axios
     .get("/api/plants/search-colors", {
       params: {
-        colors: ["赤"],
+        colors,
       },
     })
     .then((result) => {
-      console.log(result.data);
+      changeColorResults(result.data, colors);
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-searchFlowerColor();
+/**
+ * 花の色タブから、花の色を選択する
+ * @param {String} color
+ */
+const chooseFlowerColor = (color) => {
+  const colors = [];
+  colors.push(color);
+
+  getFlowerColorList(colors);
+};
 </script>
 
 <style lang="scss" scoped></style>
