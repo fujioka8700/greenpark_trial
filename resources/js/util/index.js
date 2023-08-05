365232e9d33fa53a43e4c4f128df04eed5b61e78
fujioka <fujioka8700@gmail.com>
2023-08-05 18:12:35 +0900

    [update] 花の色タブの画像を、変更した。

--- resources/js/util/index.js
+++ resources/js/util/index.js
@@ -52,3 +52,33 @@ export const plantColors = [
   "緑",
   "その他",
 ];
+
+/** @type {Array} 「花の色」タブの花の画像 */
+export const plantColorImages = [
+  // 白
+  "https://i.pinimg.com/1200x/9e/4d/b4/9e4db4faaf54b5ac51bd49603cd39a39.jpg",
+
+  // 黄
+  "https://www.medicalherb.or.jp/wp-content/uploads/2021/09/aramanda.jpg",
+
+  // 橙
+  "https://lovegreen.net/wp-content/uploads/2020/07/1d7640780a73fe25f729cab5255e40c1.jpg",
+
+  // ピンク
+  "https://yayoi-blog.info/wp-content/uploads/2017/08/tyu-rippunokisetu.jpg",
+
+  // 赤
+  "https://d2v9opmik2a3uk.cloudfront.net/uploads/2015/07/08171806/506ba69fba298ea9504f96f147c72c27.jpg",
+
+  // 青
+  "https://storage.minhana.net/wikidata/A1332/picture_users/A1332_picture_users.jpg",
+
+  // 紫
+  "https://d3pbyuzcd27kd.cloudfront.net/wp-content/uploads/sites/8/2020/02/05143857/ac78eebd9cbed6ca48cdc10dd3e5acab-e1631499860618.jpg",
+
+  // 緑
+  "https://bluegreen.jp/wp-content/uploads/2020/06/IMG_5179-1024x683.jpg",
+
+  // その他
+  "https://lovegreen.net/wp-content/uploads/2022/05/3505241_s.jpg",
+];
