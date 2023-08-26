75bcddd7997385b31ffcb9bbca1d424a22dc4a79
fujioka <fujioka8700@gmail.com>
2023-08-27 00:37:10 +0900

    [update] スマホ表示の時、フッターのリストが、縦一列になるようにした。

--- resources/js/components/modules/Footer.vue
+++ resources/js/components/modules/Footer.vue
@@ -1,15 +1,22 @@
 <template>
   <v-footer class="bg-grey-lighten-2">
     <v-row justify="center" no-gutters>
-      <v-btn
+      <v-col
         v-for="(link, index) in links"
         :key="index"
-        variant="text"
-        class="mx-3 text-capitalize"
-        @click.stop="movePage(link.dest)"
+        class="text-center"
+        cols="12"
+        sm="2"
       >
-        {{ link.title }}
-      </v-btn>
+        <v-btn
+          variant="text"
+          class="text-capitalize"
+          @click.stop="movePage(link.dest)"
+        >
+          {{ link.title }}
+        </v-btn>
+      </v-col>
+
       <v-col class="text-center mt-2 text-caption" cols="12">
         &copy; {{ new Date().getFullYear() }} GreenPark
       </v-col>
