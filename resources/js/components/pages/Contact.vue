fa9d0844c182ca43b6ed35bc2867dc0ab43dfab0
fujioka <fujioka8700@gmail.com>
2023-09-03 08:22:07 +0900

    [fix] お問い合わせメッセージ送信後、送信完了ページを表示するようにした。

--- resources/js/components/pages/Contact.vue
+++ resources/js/components/pages/Contact.vue
@@ -2,57 +2,68 @@
   <div>
     <v-sheet max-width="500" min-width="300" class="mx-auto pa-5">
       <h2>お問い合わせ</h2>
-      <p class="py-3">
-        GreenParkに対するご意見、ご感想などは、<br />こちらのフォームよりお問い合わせ下さい。
-      </p>
-      <v-form @submit.prevent="submitForm">
-        <!-- お名前 -->
-        <v-text-field
-          v-model="state.name"
-          :error-messages="v$.name.$errors.map((e) => e.$message)"
-          label="お名前"
-          required
-          @input="v$.name.$touch"
-          @blur="v$.name.$touch"
-        ></v-text-field>
+      <template v-if="!emailSent">
+        <p class="py-3">
+          GreenParkに対するご意見、ご感想などは、<br />こちらのフォームよりお問い合わせ下さい。
+        </p>
+        <v-form @submit.prevent="submitForm">
+          <!-- お名前 -->
+          <v-text-field
+            v-model="state.name"
+            :error-messages="v$.name.$errors.map((e) => e.$message)"
+            label="お名前"
+            required
+            @input="v$.name.$touch"
+            @blur="v$.name.$touch"
+          ></v-text-field>
 
-        <!-- メールアドレス -->
-        <v-text-field
-          v-model="state.email"
-          :error-messages="v$.email.$errors.map((e) => e.$message)"
-          label="メールアドレス"
-          required
-          @input="v$.email.$touch"
-          @blur="v$.email.$touch"
-        ></v-text-field>
+          <!-- メールアドレス -->
+          <v-text-field
+            v-model="state.email"
+            :error-messages="v$.email.$errors.map((e) => e.$message)"
+            label="メールアドレス"
+            required
+            @input="v$.email.$touch"
+            @blur="v$.email.$touch"
+          ></v-text-field>
 
-        <!-- お問い合わせ内容 -->
-        <v-textarea
-          v-model="state.body"
-          :error-messages="v$.body.$errors.map((e) => e.$message)"
-          variant="filled"
-          auto-grow
-          label="お問い合わせ内容"
-          rows="4"
-          @input="v$.body.$touch"
-          @blur="v$.body.$touch"
-        ></v-textarea>
+          <!-- お問い合わせ内容 -->
+          <v-textarea
+            v-model="state.body"
+            :error-messages="v$.body.$errors.map((e) => e.$message)"
+            variant="filled"
+            auto-grow
+            label="お問い合わせ内容"
+            rows="4"
+            @input="v$.body.$touch"
+            @blur="v$.body.$touch"
+          ></v-textarea>
 
-        <div class="text-center">
-          <v-btn type="submit" width="200" color="success" class="mt-5"
-            >送信する</v-btn
-          >
-        </div>
-      </v-form>
+          <div class="text-center">
+            <v-btn type="submit" width="200" color="success" class="mt-5"
+              >送信する</v-btn
+            >
+          </div>
+        </v-form>
+      </template>
+      <template v-else>
+        <p class="py-3">
+          メッセージが送信されました。<br />ありがとうございました。
+        </p>
+      </template>
     </v-sheet>
   </div>
 </template>
 
 <script setup>
 import { requiredMessage, emailMessage } from "../../plugin/validatorMessage";
-import { reactive } from "vue";
+import { ref, reactive } from "vue";
 import { useVuelidate } from "@vuelidate/core";
 import { required, email, helpers } from "@vuelidate/validators";
+
+const emailSent = ref(false);
+
+/* start of vuelidate  */
 
 const initialState = {
   name: "",
@@ -82,15 +93,23 @@ const validations = {
 
 const v$ = useVuelidate(validations, state);
 
+/* end of vuelidate  */
+
 const submitForm = async () => {
-  // 送信前にフォームを検証する
+  // 送信前に、フォームを検証する
   const isFormCorrect = await v$.value.$validate();
   if (!isFormCorrect) return;
+
+  // 送信前に、送信していいか確認する
+  if (!confirm("送信します。よろしいですか？")) return;
 
   axios
     .post("/api/contact", state)
     .then((res) => {
       console.log(res);
+
+      // メッセージ送信後の、表示に切り替える
+      emailSent.value = true;
     })
     .catch((err) => {
       console.log(err);
